055e4adb741f9ffb62d0847a1581703afd2e1e12
mrugerio <mrugerio@gmail.com>
2017-11-11 16:07:04 -0600

    Se definió el componente nota de venta.

---
+++ src/app/vertex/nota-venta/nota-venta.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-nota-venta',
+  templateUrl: './nota-venta.component.html',
+  styleUrls: ['./nota-venta.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NotaVentaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
