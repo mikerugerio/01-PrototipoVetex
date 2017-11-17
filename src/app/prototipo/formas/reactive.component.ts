caa7528c5094ccbffc5252f42897bf45df65f2df
mrugerio <mrugerio@gmail.com>
2017-11-17 09:45:37 -0600

    Se implemento componente para probar formas reactivas, tomado del curso de angular udemy. Sin funcionalidad.

---
+++ src/app/prototipo/formas/reactive.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fmReactive',
+  templateUrl: './reactive.component.html',
+  styleUrls: ['./reactive.component.css']
+})
+export class FmReactiveComponent {
+  genders = ['male', 'female'];
+}
