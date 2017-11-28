15c89c6c6754337a2a83236976a485b789211e57
mrugerio <mrugerio@gmail.com>
2017-11-28 11:25:22 -0600

    Se implemento la funcionalidad de borrar una mica de la lista

--- src/app/vertex/nota-venta/nota-venta.component.ts
+++ src/app/vertex/nota-venta/nota-venta.component.ts
@@ -101,5 +101,11 @@ export class NotaVentaComponent implements OnInit {
   addListaMicas(){
     this.valorMica = this.formaNotaVenta.get('prodMica').value;
     (this.formaNotaVenta.get('pedidoMicas') as FormArray).push(new FormControl(this.valorMica));
+    this.formaNotaVenta.controls.prodMica.setValue('');
+  }
+
+  deleteMica(mica : FormControl){
+    let index = (this.formaNotaVenta.get('pedidoMicas') as FormArray).controls.indexOf(mica);
+    (this.formaNotaVenta.get('pedidoMicas') as FormArray).removeAt(index);
   }
 }
